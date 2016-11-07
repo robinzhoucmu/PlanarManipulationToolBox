c787f84d0e3b67f7cfe4bcdc6d09daae96794e58
Jiaji Zhou <robinzhou55@gmail.com>
2016-11-07 12:02:46 -0500

    testing performance on hive for 1000 states

--- simulation/expsimulations/demo_parfor_threefinger_triangle.m
+++ simulation/expsimulations/demo_parfor_threefinger_triangle.m
@@ -1,7 +1,8 @@
 sim_results_all = cell(3, 1);
 p = gcp;
 if (isempty(p))
-    parpool(2);
+    num_physical_cores = feature('numcores');
+    parpool(num_physical_cores);
 end
 le = 0.02;
 mu = 0.1;
@@ -23,42 +24,44 @@ t_q = linspace(0, t_max, num_way_q);
 hand_traj_opts.q = waypoints_hand_q;
 hand_traj_opts.t = t_q;
 hand_traj_opts.interp_mode = 'spline';
-%[pushobj_tri,shape_info] = CreateNSidedPolygonPushObject(3, le, 'quadratic');
+[pushobj_tri,shape_info] = CreateNSidedPolygonPushObject(3, le, 'poly4');
 
 tic;
-parfor ind_pose = 1:1:3
+num_poses = 1000;
+parfor ind_pose = 1:1:num_poses
     pushobj = PushedObject(pushobj_tri.support_pts, pushobj_tri.pressure_weights, ...
         shape_info, pushobj_tri.ls_type, pushobj_tri.ls_coeffs);
-    pushobj.pose = [ind_pose* le/10; ind_pose*le/10; ind_pose*pi/6];
+    pushobj.pose = [ind_pose* le/4; ind_pose*le/4; ind_pose*pi/6];
     hand_three_finger = ConstructThreeFingersOneDofHand(finger_radius);
     hand_traj = HandTraj(hand_traj_opts);
     sim_inst = ForwardSimulationCombinedState(pushobj, hand_traj, hand_three_finger, mu);
     sim_results_all{ind_pose} = sim_inst.RollOut();
 end
-hand_for_plot = ConstructThreeFingersOneDofHand(finger_radius);
-for ind_pose = 1:1:3
-    sim_results = sim_results_all{ind_pose};
-    num_rec_configs = size(sim_results.obj_configs, 2);
-    h = figure;
-    hold on;
-    seg_size = 10;
-    for i = 1:1:num_rec_configs
-        if mod(i, seg_size) == 1
-            if (i == 1) || (i + seg_size > num_rec_configs)
-                c = 'k';
-                c_obj = 'b';
-            else
-                c = 'g';
-                c_obj = 'r';
-            end
-        % Plot the square object.
-            plot(sim_results.obj_configs(1, i), sim_results.obj_configs(2,i), 'b+');
-            vertices = SE2Algebra.GetPointsInGlobalFrame(pushobj_tri.shape_vertices, sim_results.obj_configs(:,i));
-            vertices(:,end+1) = vertices(:,1);
-            plot(vertices(1,:), vertices(2,:), '-', 'Color', c_obj);
-            hand_for_plot.Draw(h, sim_results.hand_configs(:, i), c);
-        end
-    end
-    axis equal;    
-end
 toc;
+
+% hand_for_plot = ConstructThreeFingersOneDofHand(finger_radius);
+% for ind_pose = 1:1:3
+%     sim_results = sim_results_all{ind_pose};
+%     num_rec_configs = size(sim_results.obj_configs, 2);
+%     h = figure;
+%     hold on;
+%     seg_size = 10;
+%     for i = 1:1:num_rec_configs
+%         if mod(i, seg_size) == 1
+%             if (i == 1) || (i + seg_size > num_rec_configs)
+%                 c = 'k';
+%                 c_obj = 'b';
+%             else
+%                 c = 'g';
+%                 c_obj = 'r';
+%             end
+%         % Plot the square object.
+%             plot(sim_results.obj_configs(1, i), sim_results.obj_configs(2,i), 'b+');
+%             vertices = SE2Algebra.GetPointsInGlobalFrame(pushobj_tri.shape_vertices, sim_results.obj_configs(:,i));
+%             vertices(:,end+1) = vertices(:,1);
+%             plot(vertices(1,:), vertices(2,:), '-', 'Color', c_obj);
+%             hand_for_plot.Draw(h, sim_results.hand_configs(:, i), c);
+%         end
+%     end
+%     axis equal;    
+% end
