78697dbb046f9ae15c090435f32b6e79c1515d4c
Jiaji Zhou <robinzhou55@gmail.com>
2016-11-08 15:56:24 -0500

    cylindrical sampling

---
+++ utility/cylindricalsampling.m
@@ -0,0 +1,14 @@
+% Given the radius and height of the cylinder, sample uniformly in the
+% cylindrical space. In each z-height plane (a total of num_samples_planes
+% in [-h/2, h/2]), polar coordinate based sampling (sun flower sampling)   
+function [S, x,y,theta] = cylindricalsampling(r, h, num_samples_circle, num_samples_plane)
+theta = linspace(-h/2, h/2, num_samples_plane)';
+[x, y] = sunflower_sampling(num_samples_circle, 2);
+x = r * x;
+y = r * y;
+S = zeros(3, num_samples_circle * num_samples_plane);
+for i = 1:1:num_samples_plane
+    S(:, (i-1) * num_samples_circle + 1: i*num_samples_circle) = ...
+        [x';y';theta(i) * ones(1,num_samples_circle)];
+end
+end
