05350eae99fcaf83aec59ff290bd22685d8de94a
jiajiz <jiajiz@andrew.cmu.edu>
2016-11-07 23:36:15 -0500

    some plotting

--- simulation/expsimulations/demo_parfor_threefinger_triangle.m
+++ simulation/expsimulations/demo_parfor_threefinger_triangle.m
@@ -1,5 +1,5 @@
+clear all;
 rng(1);
-sim_results_all = cell(3, 1);
 p = gcp;
 if (isempty(p))
     num_physical_cores = feature('numcores');
@@ -11,7 +11,7 @@ mu = 0.1;
 % Way points
 %% Construct hand.
 finger_radius = 0.002;
-q_start = [0; 0; pi/3;  1.25 * le + finger_radius];
+q_start = [0; 0; pi/3;  (2/sqrt(3) + 0.1) * le + finger_radius];
 q_end = [0; 0; pi/3; le/4 + finger_radius];
 
 num_way_q = 100;
@@ -25,21 +25,42 @@ t_q = linspace(0, t_max, num_way_q);
 hand_traj_opts.q = waypoints_hand_q;
 hand_traj_opts.t = t_q;
 hand_traj_opts.interp_mode = 'spline';
-[pushobj_tri,shape_info] = CreateNSidedPolygonPushObject(3, le, 'quadratic');
+%[pushobj_tri,shape_info] = CreateNSidedPolygonPushObject(3, le, 'quadratic');
 
 tic;
 num_poses = 1000;
+sim_results_all = cell(num_poses, 1);
 parfor ind_pose = 1:1:num_poses
     pushobj = PushedObject(pushobj_tri.support_pts, pushobj_tri.pressure_weights, ...
         shape_info, pushobj_tri.ls_type, pushobj_tri.ls_coeffs);
-    %pushobj.pose = [ind_pose* le/4 /num_poses; ind_pose*le/4/num_poses; ind_pose*pi/2/num_poses];
-    pushobj.pose = [le/4 * 2*(rand() - 0.5); le/4 * 2*(rand() - 0.5); 2*pi/3 * rand()];
+    % Sample a pose uniformly.
+    pushobj.pose = [le/sqrt(3) * 2 * (rand() - 0.5); le/sqrt(3) * 2  * (rand() - 0.5);  pi/3 * 2 * (rand() -0.5)];
     hand_three_finger = ConstructThreeFingersOneDofHand(finger_radius);
     hand_traj = HandTraj(hand_traj_opts);
     sim_inst = ForwardSimulationCombinedState(pushobj, hand_traj, hand_three_finger, mu);
     sim_results_all{ind_pose} = sim_inst.RollOut();
 end
 toc;
+
+h1 = figure; 
+h2 = figure;
+hold on;
+for ind_pose = 1:1:num_poses
+    q_init = sim_results_all{ind_pose}.obj_configs(:,1);
+    q_end = sim_results_all{ind_pose}.obj_configs(:,end);
+    figure(h1);
+    plot3(q_init(1)/pushobj_tri.pho, q_init(2)/pushobj_tri.pho, q_init(3), 'b*');
+    hold on;
+    figure(h2);
+    plot3(q_end(1)/pushobj_tri.pho, q_end(2)/pushobj_tri.pho, q_end(3), 'r*');
+    hold on;
+end
+
+figure; seg = 10; 
+for i = 1:1:500
+traj_obj = sim_results_all{i}.obj_configs; traj_obj(1:2,:) = traj_obj(1:2, :) / pushobj_tri.pho;
+quiver3(traj_obj(1,1:seg:end-1), traj_obj(2,1:seg:end-1), traj_obj(3,1:seg:end-1), traj_obj(1,2:seg:end) - traj_obj(1,1:seg:end-1), traj_obj(2,2:seg:end) - traj_obj(2,1:seg:end-1), traj_obj(3,2:seg:end) -traj_obj(3,1:seg:end-1) , 'MaxHeadSize', 0.2);hold on;
+end;
 
 % hand_for_plot = ConstructThreeFingersOneDofHand(finger_radius);
 % for ind_pose = 1:1:num_poses
