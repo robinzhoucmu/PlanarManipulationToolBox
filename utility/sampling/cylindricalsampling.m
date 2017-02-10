a49b28c8420157121b84f46da64ddf19ac51d6f7
Jiaji Zhou <robinzhou55@gmail.com>
2017-02-10 13:32:21 -0500

    add complete random sampling ratio in cylindraical sampling

--- utility/sampling/cylindricalsampling.m
+++ utility/sampling/cylindricalsampling.m
@@ -1,7 +1,10 @@
 % Given the radius and height of the cylinder, sample uniformly in the
 % cylindrical space. In each z-height plane (a total of num_samples_planes
 % in [-h/2, h/2]), polar coordinate based sampling (sun flower sampling)   
-function [S, x,y,theta] = cylindricalsampling(r, h, num_samples_circle, num_samples_plane)
+   function [S, x,y,theta] = cylindricalsampling(r, h, num_samples_circle, num_samples_plane, ratio_complete_random)
+if nargin < 5
+   ratio_complete_random = 0;
+end
 theta = linspace(-h/2, h/2, num_samples_plane + 1)';
 theta = theta(2:end);
 % [x, y] = sunflower_sampling(num_samples_circle, 2);
@@ -20,5 +23,11 @@ for i = 1:1:num_samples_plane
     S(:, (i-1) * num_samples_circle + 1: i*num_samples_circle) = ...
         [x';y';theta(i) * ones(1,num_samples_circle)];
 end
-
+% Random shuffling.
+rand_perm_indices = randperm(size(S, 2));
+S = S(:, rand_perm_indices);
+% Now for the specified ratio_complete_random, we randomly generate thetas.
+num_complete_random = size(S,2) * ratio_complete_random;
+thetas_random = 2 * (rand([1 num_complete_random]) - 0.5) * h/2;
+S(3, 1:num_complete_random) = thetas_random;
 end
