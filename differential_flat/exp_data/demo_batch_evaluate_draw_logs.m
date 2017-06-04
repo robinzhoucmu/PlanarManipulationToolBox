5db0e90054268f96bb4b98c65ad31775849c1dcb
jiajiz <jiajiz@andrew.cmu.edu>
2017-06-04 16:15:15 -0400

    batch eval

--- differential_flat/exp_data/demo_batch_evaluate_draw_logs.m
+++ differential_flat/exp_data/demo_batch_evaluate_draw_logs.m
@@ -1,7 +1,38 @@
-pathFolder = 'exp_data/robot_exp_logs'; 
-d = dir(pathFolder);
-isub = [d(:).isdir]; %# returns logical vector
-nameFolds = {d(isub).name}';
-nameFolds(ismember(nameFolds,{'.','..'})) = [];
+close all;
+pathFolder = {'exp_data/robot_exp_logs/bigrect_3pts', ...
+    'exp_data/robot_exp_logs/bigrect_ridge', ...
+    'exp_data/robot_exp_logs/butter', ...
+    'exp_data/robot_exp_logs/tri'};
+datafiles = {'plan_graph_bigrect_mu03nd12_cs03.mat', ...
+                    'plan_graph_bigrect_mu03nd12_cs03.mat', ...
+                    'plan_graph_butter_mu025nd10_cs05_vbhalfmm.mat', ...
+                    'plan_graph_triangle_mu03nd15.mat'};
+all_records = cell(12, 1);
+for ind_group = 1:1:4
+    load(datafiles{ind_group});
+    d = dir(pathFolder{ind_group});
+    isub = [d(:).isdir]; 
+    nameFolds = {d(isub).name}';
+    nameFolds(ismember(nameFolds,{'.','..'})) = [];
+    % loop over each folder 
+    num_folders = length(nameFolds);
+    for i = 1:1:num_folders
+        folder_name = fullfile(pathFolder{ind_group}, nameFolds{i});
+        % process all files.
+        [key_poses_obj, key_poses_robot, file_names] = read_from_execution_logs(folder_name);
+        table_center = [0; -317.5; 0];
+        num_runs = length(key_poses_obj);
+        offset_obj_final_poses = zeros(3, num_runs);
 
-% loop over each folder 
+        for i = 1:1:num_runs
+            offset_obj_final_poses(:,i) = key_poses_obj{i}(:,end) - table_center;
+            offset_obj_final_poses(3,i) = offset_obj_final_poses(3,i) * 180 / pi;
+        end
+        [mu,sigma,muci,sigmaci] = normfit(offset_obj_final_poses', 0.05);
+        ind = 4 * (ind_group - 1) + i;
+        all_records{ind}.folder_name = folder_name;
+        all_records{ind}.avg_offset = mu;
+        all_records{ind}.ci = sigmaci(2,:) - mu;
+        VisualizePushingExpLog(file_names{num_runs}, pushobj, hand_two_finger);
+    end
+end
--- differential_flat/exp_test/VisualizePushingExpLog.m
+++ differential_flat/exp_test/VisualizePushingExpLog.m
@@ -12,7 +12,7 @@ pusher_poses(:, 1:2) = pusher_poses(:, 1:2) / unit;
 action_ids = D(:,8);
 seg_draw = floor(num_stamps / 30);
 figure;
-color_map = ['b', 'm', 'c'];
+color_map = ['b', 'm', 'r'];
 num_actions = 1;
 unit = 1;
 for i = 1:1:num_stamps - 1
@@ -39,12 +39,12 @@ for i = 1:1:num_stamps - 1
         end
 
         %plot(vertices(1,:), vertices(2,:),  'Color', c, 'LineStyle', linestyle, 'LineWidth', linewidth);
-        fill(vertices(1,:), vertices(2,:), c, 'FaceAlpha', 0.1 , 'EdgeColor', 'k');
+        fill(vertices(1,:), vertices(2,:), c, 'FaceAlpha', 0.05 , 'EdgeColor', c, 'EdgeAlpha', 0.5);
         hold on;
         q = hand.q;
         q(1:3) = pusher_poses(i,:)' * unit;
         %q(4) = width_finger;
-        hand.Draw(gcf, q, 'r', 2);
+        hand.Draw(gcf, q, c, 2);
         hold on;
         if (( i == 1 || i == num_stamps - 1))
             fill(vertices(1,:), vertices(2,:), 'k', 'FaceAlpha', 0.5 , 'EdgeColor', 'k');
@@ -56,6 +56,6 @@ safe_buffer = 20;
 table_size_x = (451.6 - 100) / 1000.0;
 table_size_y = (254.0 - 10)/ 1000.0;
 table_center = [0; -317.5/1000];
-%axis([table_center(1) - table_size_x/2, table_center(1) + table_size_x/2, table_center(2) - table_size_y/2, table_center(2) + table_size_y/2]);
+axis([-0.15, 0.15, table_center(2) - 0.08, table_center(2) + 0.08]);
 axis equal;
 end
